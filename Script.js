53954094d411085016bbc9da94f5b7f1f280a1bf
Joey Patterson <JoeyPattersonBusiness@gmail.com>
2022-10-22 15:49:31 -0400

    add number button functions; update html and css

--- Script.js
+++ Script.js
@@ -1,6 +1,9 @@
+//Global Variables
 let num1 = 0;
 let num2 = 0;
+let displayValue = '';
 
+//Basic math functions
 function add(num1, num2) {
   let addValue = num1 + num2;
   let rounded = Math.round((addValue + Number.EPSILON) * 100) / 100;
@@ -29,6 +32,7 @@ function divide(num1, num2) {
   console.log("divide", rounded);
 }
 
+//Operator Function
 function operate(operator, num1, num2) {
   switch(operator) {
     case "+":
@@ -47,3 +51,47 @@ function operate(operator, num1, num2) {
       alert("ERROR: INVALID OPERATOR PASSING THROUGH SWITCH STATEMENT");
   }
 }
+
+//Number button functions
+function zero() {
+    document.getElementById("numbers").textContent += 0;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function one() {
+    document.getElementById("numbers").textContent += 1;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function two() {
+    document.getElementById("numbers").textContent += 2;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function three() {
+    document.getElementById("numbers").textContent += 3;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function four() {
+    document.getElementById("numbers").textContent += 4;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function five() {
+    document.getElementById("numbers").textContent += 5;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function six() {
+    document.getElementById("numbers").textContent += 6;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function seven() {
+    document.getElementById("numbers").textContent += 7;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function eight() {
+    document.getElementById("numbers").textContent += 8;
+    displayValue = document.getElementById("numbers").textContent;
+}
+function nine() {
+    document.getElementById("numbers").textContent += 9;
+    displayValue = document.getElementById("numbers").textContent;
+}
+
+//All operator buttons should save number before pressed
